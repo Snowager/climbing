53b315bd718a531bce6b9bcc734f403747d3623f
Cole Robinson <59709492+Snowager@users.noreply.github.com>
2024-01-15 00:29:20 -0700

    add CameraButton -create initial button for camera (no functionality yet)

--- components/cameraButton.js
+++ components/cameraButton.js
@@ -1,6 +1,29 @@
-import { Button } from "react-native"
+import { Pressable, StyleSheet, View, Dimensions } from "react-native";
 
 export default function CameraButton() {
 
-
+    return (
+        <View style={styles.cameraButtonContainer}>
+            <Pressable style={styles.cameraButton}></Pressable>
+        </View>
+    );
 };
+
+const styles = StyleSheet.create({
+    cameraButton: {
+        borderRadius: Dimensions.get('window').width * .5,
+        borderColor: "grey",
+        borderWidth: 2,
+        backgroundColor: "#444887",
+        height: "100%",
+        width: "100%",
+    },
+    cameraButtonContainer: {
+        height: 80,
+        width: 80,
+        justifyContent: "flex-end",
+        alignItems: "center",
+        zIndex: 10,
+        marginBottom: 20,
+    }
+})
