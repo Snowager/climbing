ff08b711835b5a608bb4d0524b7f1661d2722b8b
Snowager <colepr24@gmail.com>
2024-06-20 00:52:24 -0600

    add dotenv package to backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const express = require('express')
+
 
 const app = express()
 
 const port = process.env.PORT
+console.log(port)
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
