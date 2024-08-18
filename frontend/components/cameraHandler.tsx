eea4d9101f252c9079acda7b6c3cdef1bd9cbe53
Snowager <colepr24@gmail.com>
2024-08-18 13:02:52 -0600

    Update latestPhotoContainer
    - Point scrollingView at 'climbing' album
    - Fix scrollingView to update when taking picture

--- frontend/components/cameraHandler.tsx
+++ frontend/components/cameraHandler.tsx
@@ -6,22 +6,27 @@ import CameraBottomIconContainer from '../components/cameraBottomIconContainer';
 import TakePictureButton from '../components/takePictureButton';
 import * as MediaLibrary from 'expo-media-library';
 import { useAlbum } from '../hooks/useAlbum';
-import { LatestPhotoContainer } from '../components/latestPhotosContainer';
+import { ALBUM_STRING, LatestPhotoContainer } from '../components/latestPhotosContainer';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../App';
 
-type Props = NativeStackScreenProps<RootStackParamList, 'Camera'>
+// interface for CameraHandlerProps
+export interface CameraHandlerProps {
+    navigation: NativeStackScreenProps<RootStackParamList, 'Camera'>
+} 
 
-export function CameraHandler({navigation}): ReactElement<Props> {
+export function CameraHandler({navigation}): ReactElement<CameraHandlerProps> {
     const [cameraLoad, setCameraLoad] = useState(false);
     const [camera, setCamera] = useState(null);
     const [cameraType, setCameraType] = useState(CameraType.back);
     const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
     const [staticPermission, setStaticPermission] = useState(false);
-    const [albumName, setAlbumName] = useState("Hair");
+    const [climbingAssets, setClimbingAssets] = useState<MediaLibrary.Asset[]>([]);
 
-    const album = useAlbum(albumName);
+    // hook to get album async
+    const album = useAlbum(ALBUM_STRING);
 
+    // gets permission for using camera
     async function permissionFunction(): Promise<void> {
         const permission = await Camera.requestCameraPermissionsAsync();
         if (permission.granted) {
@@ -29,13 +34,10 @@ export function CameraHandler({navigation}): ReactElement<Props> {
         }
     }
 
+    // attempts to get permission on initial load
     useEffect(() => {
         permissionFunction();
     }, [])
-
-    useEffect(() => {
-        // console.log(album)
-    }, [album])
 
     useEffect(() => {
         navigation.addListener('focus', () => {
@@ -49,29 +51,47 @@ export function CameraHandler({navigation}): ReactElement<Props> {
         })
     }, [])
 
+    useEffect(() => {
+        setPictureContainer();
+    }, [])
+
     function flipCamera(): void {
         setCameraType(cameraType == CameraType.back ? CameraType.front : CameraType.back)
     }
 
+    // takes a picture and saves to library and climbing album
     async function takePicture(): Promise<void> {
         if (!camera) {
             return
         }
-        const photo = await camera.takePictureAsync();
+        const photo: MediaLibrary.Asset = await camera.takePictureAsync();
         if (permissionResponse) {
             await MediaLibrary.saveToLibraryAsync(photo.uri);
+            const asset = await MediaLibrary.createAssetAsync(photo.uri)
+            await MediaLibrary.addAssetsToAlbumAsync(asset, (await album).id)
         }
+        setPictureContainer();
     }
 
+    // handles setting scrolling picture container with album picture assets
+    const setPictureContainer = async () => {
+        const album: MediaLibrary.Album = await MediaLibrary.getAlbumAsync(ALBUM_STRING)
+        const assets: MediaLibrary.PagedInfo<MediaLibrary.Asset> = await MediaLibrary.getAssetsAsync({album: album});
+        for (let asset of assets.assets) {
+            console.log(asset.filename)
+        }
+        setClimbingAssets(assets.assets)
+    }
+
+    // handles navigation with image asset background to overlay --TODO-- make reusable export function
     function handleNavigate(image) {
         console.log(image)
         if (image) {
             navigation.navigate('Overlay', {
-                uri: image,
+                imageAsset: image,
             })
         }
     }
-
 
     const gui = useMemo(() => {
         return (
@@ -89,10 +109,10 @@ export function CameraHandler({navigation}): ReactElement<Props> {
                                 <FlipCameraButton onPress={flipCamera} />
                             </CameraBottomIconContainer>
                         </Camera>}
-                        <LatestPhotoContainer handleNavigate={handleNavigate}/>
+                        <LatestPhotoContainer handleNavigate={handleNavigate} climbingAssets={climbingAssets} cameraLoad={cameraLoad}/>
                 </View>
         )
-    }, [camera, cameraType, staticPermission, cameraLoad]
+    }, [camera, cameraType, staticPermission, cameraLoad, album, climbingAssets]
     )
 
     return (
--- frontend/components/latestPhotosContainer.tsx
+++ frontend/components/latestPhotosContainer.tsx
@@ -1,63 +1,44 @@
-import { View, Text, Image, Button } from "react-native";
-import * as imagePicker from 'expo-image-picker'
+import { Image, ScrollView, TouchableOpacity } from "react-native";
 import { ReactElement, useEffect, useState } from "react";
-import { ScrollView } from "react-native-gesture-handler";
 import * as mediaLibrary from "expo-media-library";
 
+export const ALBUM_STRING = 'climbing'
+
 export interface LatestPhotoContainerProps {
-    handleNavigate: (image) => void
+    handleNavigate: (image) => void,
+    climbingAssets: mediaLibrary.Asset[],
+    cameraLoad: boolean
 }
 
-export function LatestPhotoContainer({handleNavigate}: LatestPhotoContainerProps): ReactElement {
+// Horizontal scrolling view component that holds a scrolling list of photos from the climbing album and 
+// navigates to the overlay on tap
+export function LatestPhotoContainer({handleNavigate, climbingAssets, cameraLoad}: LatestPhotoContainerProps): ReactElement {
 
-    const [image, setImage] = useState(null);
-
-    const ALBUM_STRING = 'climbing'
-
-    const [climbingAssets, setClimbingAssets] = useState<mediaLibrary.Asset[]>([]);
-
-    useEffect(() => {
-        setPictureContainer();
-    }, [])
-
-    const setPictureContainer = async () => {
-        const album: mediaLibrary.Album = await mediaLibrary.getAlbumAsync(ALBUM_STRING)
-        const assets: mediaLibrary.PagedInfo<mediaLibrary.Asset> = await mediaLibrary.getAssetsAsync({album: album});
-        setClimbingAssets(assets.assets)
-    }
-
-    useEffect(() => {
-        if (image) {
-            handleNavigate(image)
-        }
-    }, [image])
-
-    const PickImage = async () => {
+    /*const PickImage = async () => {
         let result = await imagePicker.launchImageLibraryAsync({
             mediaTypes: imagePicker.MediaTypeOptions.Images,
             allowsEditing: true,
         })
-        setImage(result.assets[0].uri)
+        setImage(result.assets[0])
         handleNavigate(image);
-    }
-
-
-
-
+    }*/
 
     return (
         <>
-        <ScrollView horizontal={true}>
-            {climbingAssets && climbingAssets.map((asset) => {
+        <ScrollView horizontal={true} style={{width: "100%", height: "20%", backgroundColor: 'black'}}>
+            {climbingAssets && cameraLoad && climbingAssets.map((asset) => {
+                console.log(asset.uri)
                 return (
-                    <Image source={{uri: asset.uri}} style={{width: "100%", height: "20%"}} />
+                    <TouchableOpacity onPress={() => handleNavigate(asset)} >
+                        <Image source={{uri: asset.uri}} style={{width: 60, height: "100%"}} />
+                    </TouchableOpacity>
                 )
             })}
         </ScrollView>
-        <View style={{width:"100%", height:"10%", backgroundColor:"yellow", zIndex:2}}>
+        {/*<View style={{width:"100%", height:"10%", backgroundColor:"yellow", zIndex:2}}>
             {image && <Image source={{uri: image}} style={{width: "100%", height: "100%"}}/>}
             <Button title= {"Stuff here"} onPress={PickImage}/>
-        </View>
+        </View>*/}
         </>
     );
 }
