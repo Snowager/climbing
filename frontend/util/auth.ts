65994bdc0aee19659b9793a74d12715c7bf4d667
Snowager <colepr24@gmail.com>
2024-08-26 13:08:07 -0600

    Add initial createNewUser function to auth

--- frontend/util/auth.ts
+++ frontend/util/auth.ts
@@ -1,12 +1,20 @@
 import { initializeApp, FirebaseOptions } from 'firebase/app'
+import { createUserWithEmailAndPassword, getAuth, connectAuthEmulator } from 'firebase/auth'
 
 const firebaseConfig: FirebaseOptions = {
-    apiKey: null,
-    authDomain: null,
-    databaseURL: null,
-    projectId: null,
-    storageBucket: null,
-    messagingSenderId: null,
-    measurementId: null,
-    appId: null,
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.APP_DOMAIN,
 }
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export const createNewUser = async (email, password) => {
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredentials) => {
+        const user = userCredentials.user;
+    }).catch((error) => {
+        console.log(error.message);
+    })
+};
