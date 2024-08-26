4a75e35e6335e358865ed84b6c06ecb8063e3b66
Snowager <colepr24@gmail.com>
2024-08-26 11:27:11 -0600

    Add auth.ts file
    - Create util folder for util files
    - Add auth file for future auth functions
    - Create nulled firebaseConfig

---
+++ frontend/util/auth.ts
@@ -0,0 +1,12 @@
+import { initializeApp, FirebaseOptions } from 'firebase/app'
+
+const firebaseConfig: FirebaseOptions = {
+    apiKey: null,
+    authDomain: null,
+    databaseURL: null,
+    projectId: null,
+    storageBucket: null,
+    messagingSenderId: null,
+    measurementId: null,
+    appId: null,
+}
