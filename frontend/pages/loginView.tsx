9815308060ef6c0cb9940b930a16e45922e0a795
Snowager <colepr24@gmail.com>
2024-11-26 09:39:08 -0700

    Fix login route name ts error

--- frontend/pages/loginView.tsx
+++ frontend/pages/loginView.tsx
@@ -4,7 +4,7 @@ import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
 import { RootStackParamList } from '../App';
 import Login from '../components/login';
 
-type NavigationProps = BottomTabNavigationProp<RootStackParamList, 'Loginmnn'>
+type NavigationProps = BottomTabNavigationProp<RootStackParamList, 'Login'>
 
 type Props = {
     navigation: NavigationProps
